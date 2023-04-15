d729941b9c3d0aeaab5050a075e2ed934edf90a7
mimzx <105223184+mimzx@users.noreply.github.com>
2023-04-16 05:08:31 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="Add ME +",
+                text="Add Me +",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
